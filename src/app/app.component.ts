de95d807df3966613b7c36bebdcb2f4d623175fa
pavlev4040 <pavlehvr@gmail.com>
2022-01-05 10:12:36 +0100

    IT255-DZ09

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'it255-dz09';
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { VideoUrlFormComponent } from './components/video-url-form/video-url-form.component';
+import { MainFormComponent } from './components/main-form/main-form.component';
+import { VideosComponent } from './components/videos/videos.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { YoutubeService } from './services/youtube.service';
+import { VideoService } from './services/video.service';
+import { SafeIframePipe } from './helpers/safe-iframe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    VideoUrlFormComponent,
+    MainFormComponent,
+    VideosComponent,
+    SafeIframePipe,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, HttpClientModule, FormsModule],
+  providers: [YoutubeService, VideoService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
